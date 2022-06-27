23e5b1609dae19067f54225208d18eff63b4e73a
PickleDev <kd.communicate@gmail.com>
2022-06-27 22:34:15 +0800

    updated theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import "./styles.css";
+import React from "react";
+import { Box, Grommet } from "grommet";
+
+const theme = {
+  global: {
+    colors: {
+      brand: "#228BE6"
+    },
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px"
+    }
+  }
+};
+
+const AppBar = (props) => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen! ya</h2>
-    </div>
+    <Grommet theme={theme}>
+      <AppBar>Hello Grommet!</AppBar>
+    </Grommet>
   );
 }
